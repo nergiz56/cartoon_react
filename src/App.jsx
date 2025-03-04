1b661b0a6fc49a28df3adbc0ec04dcac26407398
Nergiz Şengün <nergizduygu56@gmail.com>
2025-03-04 18:18:06 +0300

    ilk commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+//import PowerGirl from './assets/bbbb.jpg'
+import './index.css'
+import Header from './Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header></Header>
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import CartoonImg from './assets/cartoon.webp';
+import './index.css';
+
+export default function Header(){
+
+    return(
+       <header>
+        <img src={CartoonImg}  alt="Cartoon Network"/>
+        <h1>CarrToon Network</h1>
+        <p>Cartoon Network is a popular television channel founded in 1992, broadcasting animated series for children. 🚀</p>
+       </header>
+    );
+}
